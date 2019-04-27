78243e58ba61656b3daea4754ac8f40ccb79f5b1
tourman <8002615@gmail.com>
2019-04-27 14:30:10 +0300

    fresh build

---
+++ tart/precache-manifest.09aa05c79bc0f3adda692dd5a0c60653.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ee012452ec97839ea7e",
+    "url": "/tart/static/css/main.dc278749.chunk.css"
+  },
+  {
+    "revision": "2ee012452ec97839ea7e",
+    "url": "/tart/static/js/main.6ff490f4.chunk.js"
+  },
+  {
+    "revision": "ce1e57c8326e166136b6",
+    "url": "/tart/static/js/runtime~main.99bef1e6.js"
+  },
+  {
+    "revision": "06671cb618874d5e8e52",
+    "url": "/tart/static/js/2.af26b250.chunk.js"
+  },
+  {
+    "revision": "b6815fa8232c94563f97bd3a56dde9be",
+    "url": "/tart/index.html"
+  }
+];
