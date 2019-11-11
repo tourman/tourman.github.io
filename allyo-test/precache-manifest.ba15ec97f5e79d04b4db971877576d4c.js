9d0e5e50c9817c038f58810ae76a17dd397e5062
tourman <8002615@gmail.com>
2019-11-11 09:58:36 +0200

    allyo-test

---
+++ allyo-test/precache-manifest.ba15ec97f5e79d04b4db971877576d4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f761d001e369714eddf63b403f81515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50b07a1d0b3003ad6b41",
+    "url": "/static/css/main.2d9f764c.chunk.css"
+  },
+  {
+    "revision": "a01088121ab9cb4eef4a",
+    "url": "/static/js/2.dc8ccce7.chunk.js"
+  },
+  {
+    "revision": "50b07a1d0b3003ad6b41",
+    "url": "/static/js/main.4eaa6bd4.chunk.js"
+  },
+  {
+    "revision": "ef3f9b9288d8c344924b",
+    "url": "/static/js/runtime-main.b332fca4.js"
+  }
+]);
---
+++ allyo-test/precache-manifest.d72a55f557f09e15240708bbe0a152be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b4afcc836f55a669ad1b8ab6d201ba",
+    "url": "/allyo-test/index.html"
+  },
+  {
+    "revision": "33940e993521ee42855a",
+    "url": "/allyo-test/static/css/main.2d9f764c.chunk.css"
+  },
+  {
+    "revision": "a01088121ab9cb4eef4a",
+    "url": "/allyo-test/static/js/2.dc8ccce7.chunk.js"
+  },
+  {
+    "revision": "33940e993521ee42855a",
+    "url": "/allyo-test/static/js/main.e011c085.chunk.js"
+  },
+  {
+    "revision": "984715a1d8133de4ecc8",
+    "url": "/allyo-test/static/js/runtime-main.b527d20a.js"
+  }
+]);
